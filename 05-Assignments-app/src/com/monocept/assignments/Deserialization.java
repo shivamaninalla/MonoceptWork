cd499b57131b0e53a483b3f2bde4aa2ab6c5436e
nallashivamani <20eg106220@anurag.edu.in>
2024-06-03 21:35:02 +0530

    Desrialization

--- 05-Assignments-app/src/com/monocept/assignments/Deserialization.java
+++ 05-Assignments-app/src/com/monocept/assignments/Deserialization.java
@@ -1,10 +1,33 @@
 package com.monocept.assignments;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Deserialization {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		SavingsAccount savingsAccount=null;
+		try {
+			
+			FileInputStream fileInput=new FileInputStream("C:\\Users\\shiva\\OneDrive\\Desktop\\workspace\\05-Assignments-app\\src\\com\\monocept\\assignments\\SerializationFile");
+		    ObjectInputStream in =new ObjectInputStream(fileInput);
+		    savingsAccount=(SavingsAccount)in.readObject();
+		    in.close();
+		    fileInput.close();
+		    
+		    
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+		    System.out.println("Desirializing SavingsAccount");
+		    System.out.println("Account Number: "+savingsAccount.getAccountNumber());
+		    System.out.println("Account name: "+savingsAccount.getName());
+		    System.out.println("Account balance: "+savingsAccount.getBalance());
+		    }
 	}
 
 }
